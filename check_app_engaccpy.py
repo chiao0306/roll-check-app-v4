8c8994a7cbfd55984330f8824a437615b745ccdc
chiao0306 <ciaoyuyang@gmail.com>
2026-01-04 23:48:52 +0800

    Update check_app_engaccpy.py

--- check_app_engaccpy.py
+++ check_app_engaccpy.py
@@ -1043,7 +1043,7 @@ def python_accounting_audit(dimension_data, res_main):
                 continue 
             
             # --- 模式匹配 ---
-            match_A = (fuzz.partial_ratio(s_clean, title_clean) > 90)
+            match_A = (fuzz.partial_ratio(s_clean, title_clean) > 95)
             
             match_B = False
             is_dis = "ROLL拆裝" in s_clean
